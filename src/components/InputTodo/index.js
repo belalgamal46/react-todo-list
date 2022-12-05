2b9aa464c13d5511e2b54f0723d6f02f268fe9a7
belalgamal46 <belalgamal00@gmail.com>
2022-12-05 19:12:43 +0200

    Add adding todo functionality

--- src/components/InputTodo/index.js
+++ src/components/InputTodo/index.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import './InputTodo.css';
 
-const InputTodo = () => {
-  return <div>InputTodo</div>;
+const InputTodo = ({ handleTasksChange }) => {
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const title = event.target.children[0].value;
+    handleTasksChange(title);
+    event.target.reset();
+  };
+
+  return (
+    <section className="input-container">
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="todo" id="todo-input" placeholder="Add todo..." />
+        <button type="submit" id="add-todo-btn">
+          <img src="images/add-icon.png" alt="add-icon" />
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default InputTodo;
