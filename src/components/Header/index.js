225fbd51535dee227985edafe6cf9b1083090065
belalgamal46 <belalgamal00@gmail.com>
2023-01-09 09:04:53 +0200

    Add header and refactor code

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import styles from './Header.module.css';
+
+const Header = () => (
+  <section className={styles.title_container}>
+    <h1 className={styles.title}>Todos</h1>
+  </section>
+);
+
+export default Header;
